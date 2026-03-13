1961ea59b2a58bcf5d3e4dcc371dc53f9de1eaa3
Michael Boggess <coda5351@gmail.com>
2026-03-13 17:12:43 -0400

    consolidated calls to one passing event name sending same payload

--- spring/src/test/java/com/waitlist/controller/WaitlistControllerTest.java
+++ spring/src/test/java/com/waitlist/controller/WaitlistControllerTest.java
@@ -1,19 +1,15 @@
 package com.waitlist.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.waitlist.model.Account;
 import com.waitlist.service.AccountService;
 import com.waitlist.service.EntryService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -27,9 +23,6 @@ public class WaitlistControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @MockBean
     private AccountService accountService;
